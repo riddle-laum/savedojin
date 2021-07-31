a5b34319eeb9f02b0016c9f981e2dcda43050d2e
riddle-laum <67580233+riddle-laum@users.noreply.github.com>
2021-08-01 04:52:28 +0900

    add

--- local/main.js
+++ local/main.js
@@ -223,7 +223,7 @@ savedojin.modules = {
       for(var dom of document.querySelectorAll('.kijibox img'))
         if(dom.srcset) r.urls.push(dom.srcset);
         else if(dom.dataset.lazySrcset) r.urls.push(dom.dataset.lazySrcset);
-      r.urls = srcsetParse(r.urls);
+      r.urls = savedojin.assets.srcsetParse(r.urls);
       r.title = 'eromanga-sokuho-' + location.href.split(/\/|\./)[4];
       return r;
     }
