82a8ebe5c2ea5780595fb009b9ff61446a4a31b2
riddle-laum <67580233+riddle-laum@users.noreply.github.com>
2021-08-01 05:22:46 +0900

    add

--- local/main.js
+++ local/main.js
@@ -199,7 +199,7 @@ savedojin.modules = {
       for(var dom of document.querySelectorAll('.description > a > img')) r.urls.push(dom.srcset);
       if(r.urls.length == 0)
         for(var dom of document.querySelectorAll('.description > img')) r.urls.push(dom.srcset);
-      r.urls = srcsetParse(r.urls);
+      r.urls = savedojin.assets.srcsetParse(r.urls);
       var locate = location.href.split(/\/|\./g);
       r.title = locate[locate.length - 1];
       if(r.title == 'html' || r.title == '') r.title = locate[locate.length - 2];
